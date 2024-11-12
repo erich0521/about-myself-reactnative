347176b211f95f4f0305ea4aeeff0ffddc6d5654
erich0521 <erichnicolecastillo@gmail.com>
2024-11-12 14:05:30 -0600

    Updated

---
+++ Documents/OOP_DBMS/BakeShopSystem/src/bakeshopsystem/BakeShopSystem.java
@@ -0,0 +1,19 @@
+
+package bakeshopsystem;
+
+import UI.LoginFrame;
+
+public class BakeShopSystem {
+
+
+    public static void main(String[] args) {
+        
+        LoginFrame LoginFrame = new LoginFrame();
+        LoginFrame.setVisible(true);
+        LoginFrame.pack();
+        LoginFrame.setLocationRelativeTo(null);
+        
+        
+    }
+    
+}
